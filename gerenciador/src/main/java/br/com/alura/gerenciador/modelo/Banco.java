3df5b02d3a6c8f70afe6a8af9bc819483777e4df
Nelson <punkpunkada@gmail.com>
2022-07-10 15:06:50 -0300

    import bug fix

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/modelo/Banco.java
@@ -0,0 +1,77 @@
+package br.com.alura.gerenciador.modelo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Banco {
+	
+	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> usuarios = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
+		empresa.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
+		empresa2.setNome("Caelum");
+		lista.add(empresa);
+		lista.add(empresa2);
+		
+		
+		Usuario usuario1 = new Usuario();
+		usuario1.setLogin("Nelson");
+		usuario1.setSenha("12345");
+		
+		Usuario usuario2 = new Usuario();
+		usuario2.setLogin("Noeme");
+		usuario2.setSenha("54321");
+		
+		usuarios.add(usuario1);
+		usuarios.add(usuario2);
+		
+	}
+
+	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
+		Banco.lista.add(empresa);
+	}
+	
+	public List<Empresa> getEmpresas(){
+		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			
+			if(emp.getId() == id) {
+				it.remove();
+			}
+		}
+	}
+
+	public Empresa buscaEmpresaPelaId(Integer id) {
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
+	}
+	
+	public Usuario existeUsuario(String login, String senha) {
+		for(Usuario usuario : usuarios) {
+			if(usuario.ehIgual(login, senha)) {
+				return usuario;
+			}
+		}
+		return null;
+	}
+
+}
